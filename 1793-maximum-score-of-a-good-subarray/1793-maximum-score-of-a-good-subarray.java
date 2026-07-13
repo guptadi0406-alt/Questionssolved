38e96fd841e8e0ec2d090bc495430ff769f23885
Aditya gupta <guptadi0406@gmail.com>
2026-07-13 17:55:12 +0530

    Time: 165 ms (15.58%), Space: 86.8 MB (13.88%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
@@ -24,32 +24,31 @@ class Solution {
 
         }
 
-        Stack<Integer> st2 = new Stack<>();
+       st1.clear();
 
         for(int i=n-1;i>=0;i--){
 
-                while(!st2.isEmpty() && nums[st2.peek()]>=nums[i]){
-                    st2.pop();
+                while(!st1.isEmpty() && nums[st1.peek()]>=nums[i]){
+                    st1.pop();
                 }
 
 
-                if(st2.isEmpty()){
+                if(st1.isEmpty()){
                     nextmin[i]=n;
                 }else{
-                    nextmin[i]=st2.peek();
+                    nextmin[i]=st1.peek();
                 }
-                st2.push(i);
+                st1.push(i);
 
         }
 
-        // System.out.println(Arrays.toString(prevmin));
-        // System.out.println(Arrays.toString(nextmin));
+        
 
         int ans = 0;
         for(int i=0;i<n;i++){
 
             if(nextmin[i]-1>=k && prevmin[i]+1<=k){
-                // System.out.println(nums[i]*(nextmin[i]-prevmin[i]-1));
+             
                 ans = Math.max(ans , nums[i]*(nextmin[i]-prevmin[i]-1));
             }
         }
