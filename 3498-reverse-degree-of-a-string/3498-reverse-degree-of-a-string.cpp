49c27f5f5be7c9f2c748cc027cec1f71af3794c2
Aditya gupta <guptadi0406@gmail.com>
2026-09-20 16:06:04 +0530

    Time: 0 ms (100%), Space: 9.6 MB (78.4%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -3,14 +3,10 @@ public:
     int reverseDegree(string s) {
         int ans =0;
         int n = s.length();
-        map<char,int> mp;
 
-        for(int i=1;i<=26;i++){
-            mp['a'+26-i] = i;
-        }
 
         for(int i=0;i<n;i++){
-            ans += (i+1)*(mp[s[i]]);
+            ans += (i+1)*('a'+26-s[i]);
         }
 
         return ans ;
